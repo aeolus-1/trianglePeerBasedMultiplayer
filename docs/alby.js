6f7fe7752311f700ab87f82de078de43f024143a
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-04-24 11:03:24 +1000

    Update alby.js

--- docs/alby.js
+++ docs/alby.js
@@ -9,16 +9,27 @@ ably.connection.on("connected", () => {
 });
 
 var onlinePlayers = new Object(),
+    onlinePlayersComp = Matter.Composite.create()
     onlineId = `${Math.random()}`
+
+Matter.Composite.add(engine.world, onlinePlayersComp)
 
 var channel = ably.channels.get("game");
 channel.subscribe("gamePlayers", (message) => {
   var dat = JSON.parse(message.data)
+    if (dat.id != onlineId) {
+    if (onlinePlayers[dat.id] == undefined) {
+        onlinePlayers[dat.id] = {body:addOnlinePlayer(dat)}
+        
+        console.log("add")
+    }
 
-  onlinePlayers[dat.id] = {}
+        Matter.Body.set(onlinePlayers[dat.id].body, "position", dat.position)
+        Matter.Body.set(onlinePlayers[dat.id].body, "velocity", dat.velocity)
 
-    onlinePlayers[dat.id].position = dat.position
-  onlinePlayers[dat.id].angle = dat.angle
+        Matter.Body.set(onlinePlayers[dat.id].body, "angle", dat.angle)
+        Matter.Body.set(onlinePlayers[dat.id].body, "anglarVelocity", dat.anglarVelocity)
+}
 
 });
 
@@ -26,12 +37,30 @@ function broadcastData() {
     channel.publish("gamePlayers", JSON.stringify({
         id:onlineId,
         position:{...entitys[0].body.position},
-        angle:entitys[0].body.angle
+        velocity:{...entitys[0].body.velocity},
+        angle:entitys[0].body.angle,
+        anglarVelocity:entitys[0].body.anglarVelocity,
     }))
+}
+
+function addOnlinePlayer(dat) {
+    var body = Matter.Bodies.rectangle(dat.position.x, dat.position.y, 30, 30,{
+        frictionAir:0,
+        restitution:0.15,
+        friction:0.7,
+        frictionStatic:0,
+        render:{
+            fillStyle:colorTheme.player
+        }
+    })
+
+    Matter.Composite.add(onlinePlayersComp, body)
+
+    return body
 }
 
 setInterval(() => {
     if (broadCasting) {
         broadcastData()
     }
-}, 100);
+}, 56);
